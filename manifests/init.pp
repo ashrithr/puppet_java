58bc6b291aab66bf5a401222027544404e041cfa
ashrithr <ashrith@me.com>
2013-09-10 14:11:43 -0700

    Template rename

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,56 +27,56 @@ class java {
   require java::params
 
   file {"$java::params::java_base":
-    ensure => "directory",
-    owner => "root",
-    group => "root",
-    alias => "java-base"
+    ensure  => "directory",
+    owner   => "root",
+    group   => "root",
+    alias   => "java-base"
   }
 
   file { "${java::params::java_base}/jdk${java::params::java_version}.tar.gz":
-    mode => 0644,
-    owner => root,
-    group => root,
-    source => "puppet:///modules/java/jdk${java::params::java_version}.tar.gz",
-    alias => "java-source-tgz",
-    before => Exec["untar-java"],
+    mode    => 0644,
+    owner   => root,
+    group   => root,
+    source  => "puppet:///modules/java/jdk${java::params::java_version}.tar.gz",
+    alias   => "java-source-tgz",
+    before  => Exec["untar-java"],
     require => File["java-base"]
   }
 
   exec { "untar jdk${java::params::java_version}.tar.gz":
-    command => "/bin/tar -zxf jdk${java::params::java_version}.tar.gz",
-    cwd => "${java::params::java_base}",
-    creates => "${java::params::java_base}/jdk${java::params::java_version}",
-    alias => "untar-java",
+    command     => "/bin/tar -zxf jdk${java::params::java_version}.tar.gz",
+    cwd         => "${java::params::java_base}",
+    creates     => "${java::params::java_base}/jdk${java::params::java_version}",
+    alias       => "untar-java",
     refreshonly => true,
-    subscribe => File["java-source-tgz"],
-    before => File["java-app-dir"]
+    subscribe   => File["java-source-tgz"],
+    before      => File["java-app-dir"]
   }
 
   file { "${java::params::java_base}/jdk${java::params::java_version}":
-    ensure => "directory",
-    mode => 0644,
-    owner => root,
-    group => root,
-    alias => "java-app-dir"
+    ensure  => "directory",
+    mode    => 0644,
+    owner   => root,
+    group   => root,
+    alias   => "java-app-dir"
   }
 
-  file { "/etc/profile.d/set_java_home.sh":
-    ensure	=>	present,
-    content	=>	template("java/set_java_home.sh.erb"),
-    mode	=> 0755,
-    owner	=> root,
-    group   => root,
-    require	=>	File["java-app-dir"],
+  file { "/etc/profile.d/java_home.sh":
+    ensure    =>	present,
+    content	  =>	template("java/java_home.sh.erb"),
+    mode      => 0755,
+    owner	    => root,
+    group     => root,
+    require	  =>	File["java-app-dir"],
   }
   
   #FIX: This causes java to source every time
   exec { "source-java":
-    command	=> "/etc/profile.d/set_java_home.sh | sh",
-    user    => 'root',
-    group   => 'root',
+    command	  => "/etc/profile.d/java_home.sh | sh",
+    user      => 'root',
+    group     => 'root',
     logoutput => 'true',
-    onlyif => '/usr/bin/test -z $JAVA_HOME', #run only if this command returns 0 
-    require	=>	File ["/etc/profile.d/set_java_home.sh"],
+    onlyif    => '/usr/bin/test -z $JAVA_HOME',
+    require	  =>	File ["/etc/profile.d/java_home.sh"],
   }
 }
